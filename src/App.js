e215aa6c39b51954ee57a4b2f662fbc089054088
Rdritik <ritikkumar2714@gmail.com>
2021-05-23 14:19:27 +0530

    Created Board and  Square Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
+import Board from "./components/Board";
 
 const App = () =>{
   return(
-    <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal</p>
-    <ul>
-      <li><h1>Ritik Kumar</h1></li>
-    </ul>
-  </>
+    <div>
+    <h1>TIC TAC TOE</h1>
+    < Board/>
+  </div>
   );
 };
 
