d7836e33bccf92ccadce349820c229faa3653b90
Rdritik <ritikkumar2714@gmail.com>
2021-05-23 09:56:38 +0530

    git init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () =>{
+  return(
+    <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal</p>
+    <ul>
+      <li><h1>Ritik Kumar</h1></li>
+    </ul>
+  </>
+  );
+};
+
+
+export default App;
