54a12ac6c78fbd26f68bd9d76a6f385fa8cee623
Rdritik <ritikkumar2714@gmail.com>
2021-05-23 16:55:37 +0530

    added board state and also react events

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from "react";
-import Board from "./components/Board";
+import React from 'react';
+import Board from './components/Board';
 import './styles/root.scss';
 
-const App = () =>{
-  return(
-    <div className='app'>
-    <h1>TIC TAC TOE</h1>
-    < Board/>
-  </div>
+const App = () => {
+  return (
+    <div className="app">
+      ♀<h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
   );
 };
-
 
 export default App;
